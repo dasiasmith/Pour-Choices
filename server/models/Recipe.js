e9903061d875179b249d7cafa1764f54e3a35097
Jing Liu <jingliu778@gmail.com>
2022-04-07 19:18:22 -0400

    set model

--- server/models/Recipe.js
+++ server/models/Recipe.js
@@ -1,17 +1,29 @@
-const { Schema, model } = require('mongoose');
-const dateFormat = require('../utils/dateFormat');
+const { Schema, model } = require("mongoose");
+const dateFormat = require("../utils/dateFormat");
 
-const thoughtSchema = new Schema({
-  thoughtText: {
+const recipeSchema = new Schema({
+  recipeName: {
     type: String,
-    required: 'You need to leave a thought!',
+    required: "You need to leave a name!",
     minlength: 1,
     maxlength: 280,
     trim: true,
   },
-  thoughtAuthor: {
+  // recipeAuthor: {
+  //   type: String,
+  //   required: true,
+  //   trim: true,
+  // },
+  ingredients: {
     type: String,
-    required: true,
+    required: "You need to create a ingredients!",
+    minlength: 1,
+    trim: true,
+  },
+  instructions: {
+    type: String,
+    required: "You need to create a instructions!",
+    minlength: 1,
     trim: true,
   },
   createdAt: {
@@ -19,27 +31,27 @@ const thoughtSchema = new Schema({
     default: Date.now,
     get: (timestamp) => dateFormat(timestamp),
   },
-  comments: [
-    {
-      commentText: {
-        type: String,
-        required: true,
-        minlength: 1,
-        maxlength: 280,
-      },
-      commentAuthor: {
-        type: String,
-        required: true,
-      },
-      createdAt: {
-        type: Date,
-        default: Date.now,
-        get: (timestamp) => dateFormat(timestamp),
-      },
-    },
-  ],
+  // comments: [
+  //   {
+  //     commentText: {
+  //       type: String,
+  //       required: true,
+  //       minlength: 1,
+  //       maxlength: 280,
+  //     },
+  //     commentAuthor: {
+  //       type: String,
+  //       required: true,
+  //     },
+  //     createdAt: {
+  //       type: Date,
+  //       default: Date.now,
+  //       get: (timestamp) => dateFormat(timestamp),
+  //     },
+  //   },
+  // ],
 });
 
-const Thought = model('Thought', thoughtSchema);
+const Recipe = model("Recipe", recipeSchema);
 
-module.exports = Thought;
+module.exports = Recipe;
