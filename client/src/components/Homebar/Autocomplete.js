207900eb987dba8b057d509557dcbed1274c628c
John Quintanilla <jq150010@gmail.com>
2022-04-11 15:02:31 -0400

    added the cocktail lookup function and then rendered cocktails with pictures

--- client/src/components/Homebar/Autocomplete.js
+++ client/src/components/Homebar/Autocomplete.js
@@ -11,47 +11,79 @@ const checkedIcon = <CheckBoxIcon fontSize="small" />;
 
 export default function CheckboxesTags() {
 
-  const [data, setData] = useState([]);
-  const [selected, setSelected] = useState([]);
+    const [data, setData] = useState([]);
+    const [list, setList] = useState([]);
+    const [selected, setSelected] = useState([]);
+    const [title, setTitle] = useState([]);
 
-  useEffect(() => {
-    fetch("https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list")
-    .then(res => res.json())
-    .then(data => {
-       let newData = data.drinks;
-       newData = newData.map(ele => {
-         const title = ele.strIngredient1;
-         return { "title": title };
-       })
-       setData(newData)
-    });
-  },[]);
-  console.log("my selected: ", selected);
-  return (
-    <Autocomplete
-      multiple
-      id="checkboxes-tags-demo"
-      options={data}
-      onChange={(e, newValue) => {
-        setSelected(newValue);
-      }}
-      disableCloseOnSelect
-      getOptionLabel={(option) => option.title}
-      renderOption={(props, option, { selected }) => (
-        <li {...props}>
-          <Checkbox
-            icon={icon}
-            checkedIcon={checkedIcon}
-            style={{ marginRight: 8 }}
-            checked={selected}
-          />
-          {option.title}
-        </li>
-      )}
-      style={{ width: 500 }}
-      renderInput={(params) => (
-        <TextField {...params} label="Checkboxes" placeholder="Favorites" />
-      )}
-    />
-  );
+    //get list of ingredients for the user
+    useEffect(() => {
+        fetch("https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list")
+            .then(res => res.json())
+            .then(data => {
+                let newData = data.drinks;
+                newData = newData.map(ele => {
+                    const title = ele.strIngredient1;
+                    return { "title": title };
+                })
+                setData(newData)
+            });
+    }, []);
+    console.log("my selected: ", selected);
+
+    const fetchList = () => {
+        const query = selected.map((element) => element.title).join();
+        console.log('query ===', query);
+        fetch(`https://www.thecocktaildb.com/api/json/v2/9973533/filter.php?i=${query}`)
+            .then(res => res.json())
+            .then(data => {
+                let newData2 = data.drinks
+                console.log(data.drinks);
+                if (newData2.length && newData2 !== 'None Found') {
+                    setList(newData2);
+                } else {
+                    setList([]);
+                }
+            });
+    }
+
+    return (
+        <>
+            <Autocomplete
+                multiple
+                id="checkboxes-tags-demo"
+                options={data}
+                onChange={(e, newValue) => {
+                    setSelected(newValue);
+                    fetchList();
+                }}
+                disableCloseOnSelect
+                getOptionLabel={(option) => option.title}
+                renderOption={(props, option, { selected }) => (
+                    <li {...props}>
+                        <Checkbox
+                            icon={icon}
+                            checkedIcon={checkedIcon}
+                            style={{ marginRight: 8 }}
+                            checked={selected}
+                        />
+                        {option.title}
+                    </li>
+                )}
+                style={{ width: 500 }}
+                renderInput={(params) => (
+                    <TextField {...params} label="Checkboxes" placeholder="Favorites" />
+                )}
+            />
+
+            <div className='row'>
+                {list.length > 0 ? list.map((e) => (
+                    <div key={e.idDrink}>
+                        <p>{e.strDrink}</p>
+                        <img src={e.strDrinkThumb} alt="" width="40px" height="40px" />
+                    </div>
+                )) : (<div><p style={{ color: 'white' }}>no data</p></div>)}
+            </div>
+        </>
+    );
 }
